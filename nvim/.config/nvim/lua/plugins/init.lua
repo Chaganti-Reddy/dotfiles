6b007465b181b395ab33fc7c6e83ceac999a7dba
Chaganti-Reddy <chagantivenkataramireddy4@gmail.com>
2025-01-29 19:25:32 +0530

    update nvim/.config/nvim/lua/plugins/init.lua

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -1,253 +1,328 @@
--- All plugins have lazy=true by default,to load a plugin on startup just lazy=false
--- List of all default plugins & their definitions
-local default_plugins = {
+return {
+  {
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre', -- uncomment for format on save
+    opts = require "configs.conform",
+  },
 
   "nvim-lua/plenary.nvim",
 
   {
-    "NvChad/base46",
-    branch = "v2.0",
+    "nvchad/ui",
+    config = function()
+      require "nvchad"
+    end,
+  },
+
+  {
+    "nvchad/base46",
+    lazy = true,
     build = function()
       require("base46").load_all_highlights()
     end,
   },
 
-  {
-    "NvChad/ui",
-    branch = "v2.0",
-    lazy = false,
-  },
+  "nvzone/volt",
 
+  -- These are some examples, uncomment them if you want to see them work!
   {
-    "NvChad/nvterm",
-    init = function()
-      require("core.utils").load_mappings "nvterm"
-    end,
-    config = function(_, opts)
-      require "base46.term"
-      require("nvterm").setup(opts)
-    end,
-  },
-
-  {
-    "NvChad/nvim-colorizer.lua",
-    event = "User FilePost",
-    config = function(_, opts)
-      require("colorizer").setup(opts)
-
-      -- execute colorizer as soon as possible
-      vim.defer_fn(function()
-        require("colorizer").attach_to_buffer(0)
-      end, 0)
-    end,
-  },
-
-  {
-    "nvim-tree/nvim-web-devicons",
-    opts = function()
-      return { override = require "nvchad.icons.devicons" }
-    end,
-    config = function(_, opts)
-      dofile(vim.g.base46_cache .. "devicons")
-      require("nvim-web-devicons").setup(opts)
-    end,
-  },
-
-  {
-    "lukas-reineke/indent-blankline.nvim",
-    version = "2.20.7",
-    event = "User FilePost",
-    opts = function()
-      return require("plugins.configs.others").blankline
-    end,
-    config = function(_, opts)
-      require("core.utils").load_mappings "blankline"
-      dofile(vim.g.base46_cache .. "blankline")
-      require("indent_blankline").setup(opts)
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "configs.lspconfig"
     end,
   },
 
   {
     "nvim-treesitter/nvim-treesitter",
-    event = { "BufReadPost", "BufNewFile" },
-    tag = "v0.9.2",
-    cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSModuleInfo" },
-    build = ":TSUpdate",
-    opts = function()
-      return require "plugins.configs.treesitter"
-    end,
-    config = function(_, opts)
-      dofile(vim.g.base46_cache .. "syntax")
-      require("nvim-treesitter.configs").setup(opts)
-    end,
+    opts = {
+      ensure_installed = {
+        "vim",
+        "lua",
+        "vimdoc",
+        "html",
+        "css",
+      },
+    },
   },
 
-  -- git stuff
-  {
-    "lewis6991/gitsigns.nvim",
-    event = "User FilePost",
-    opts = function()
-      return require("plugins.configs.others").gitsigns
-    end,
-    config = function(_, opts)
-      dofile(vim.g.base46_cache .. "git")
-      require("gitsigns").setup(opts)
-    end,
-  },
 
-  -- lsp stuff
-  {
-    "williamboman/mason.nvim",
-    cmd = { "Mason", "MasonInstall", "MasonInstallAll", "MasonUpdate" },
-    opts = function()
-      return require "plugins.configs.mason"
-    end,
-    config = function(_, opts)
-      dofile(vim.g.base46_cache .. "mason")
-      require("mason").setup(opts)
-
-      -- custom nvchad cmd to install all mason binaries listed
-      vim.api.nvim_create_user_command("MasonInstallAll", function()
-        if opts.ensure_installed and #opts.ensure_installed > 0 then
-          vim.cmd("MasonInstall " .. table.concat(opts.ensure_installed, " "))
-        end
-      end, {})
-
-      vim.g.mason_binaries_list = opts.ensure_installed
-    end,
-  },
 
   {
-    "neovim/nvim-lspconfig",
-    event = "User FilePost",
-    config = function()
-      require "plugins.configs.lspconfig"
-    end,
-  },
-
-  -- load luasnips + cmp related in insert mode only
-  {
-    "hrsh7th/nvim-cmp",
+    "max397574/better-escape.nvim",
     event = "InsertEnter",
+    config = function()
+      require("better_escape").setup()
+    end,
+  },
+
+  { "ellisonleao/glow.nvim", config = true, cmd = "Glow" },
+
+  {
+    "stevearc/conform.nvim",
+    --  for users those who want auto-save conform + lazyloading!
+    -- event = "BufWritePre"
+    config = function()
+      require "configs.conform"
+    end,
+  },
+
+  -- LeetCode in Neovim
+  -- {
+  --     "kawre/leetcode.nvim",
+  --     lazy = false,
+  --     build = ":TSUpdate html",
+  --     dependencies = {
+  --       "nvim-telescope/telescope.nvim",
+  --       "nvim-lua/plenary.nvim", -- required by telescope
+  --       "MunifTanjim/nui.nvim",
+  --       "rcarriga/nvim-notify",
+  --
+  --       -- optional
+  --       "nvim-treesitter/nvim-treesitter",
+  --       "nvim-tree/nvim-web-devicons",
+  --     },
+  --     keys = {
+  --       { "<leader>lq", mode = { "n" }, "<cmd>Leet tabs<cr>" },
+  --       { "<leader>lm", mode = { "n" }, "<cmd>Leet menu<cr>" },
+  --       { "<leader>lc", mode = { "n" }, "<cmd>Leet console<cr>" },
+  --       { "<leader>li", mode = { "n" }, "<cmd>Leet info<cr>" },
+  --       { "<leader>ll", mode = { "n" }, "<cmd>Leet lang<cr>" },
+  --       { "<leader>ld", mode = { "n" }, "<cmd>Leet desc<cr>" },
+  --       { "<leader>lr", mode = { "n" }, "<cmd>Leet run<cr>" },
+  --       { "<leader>ls", mode = { "n" }, "<cmd>Leet submit<cr>" },
+  --       { "<leader>ly", mode = { "n" }, "<cmd>Leet yank<cr>" },
+  --       { "<leader>lp", mode = { "n" }, "<cmd>Leet list<cr>" },
+  --     },
+  --     config = function()
+  --       require "custom.configs.leetcode"
+  --     end,
+  --   },
+
+  -- Codeforces in Neovim
+  {
+    "xeluxee/competitest.nvim",
+    lazy = false,
+    dependencies = "MunifTanjim/nui.nvim",
+    config = function()
+      require "configs.competitest"
+    end,
+  },
+
+  -- Compiler in Neovim
+
+  { -- This plugin
+    "Zeioth/compiler.nvim",
+    cmd = { "CompilerOpen", "CompilerToggleResults", "CompilerRedo" },
+    dependencies = { "stevearc/overseer.nvim", "nvim-telescope/telescope.nvim" },
+    opts = {},
+  },
+  { -- The task runner we use
+    "stevearc/overseer.nvim",
+    commit = "6271cab7ccc4ca840faa93f54440ffae3a3918bd",
+    cmd = { "CompilerOpen", "CompilerToggleResults", "CompilerRedo" },
+    opts = {
+      task_list = {
+        direction = "bottom",
+        min_height = 25,
+        max_height = 25,
+        default_detail = 1
+      },
+    },
+  },
+
+  -- Github Copilot in Neovim
+  -- {
+  --   "zbirenbaum/copilot.lua",
+  --   cmd = "Copilot",
+  --   event = "InsertEnter",
+  --   config = function()
+  --     require("copilot").setup {
+  --       panel = {
+  --         enabled = true,
+  --         auto_refresh = true,
+  --         keymap = {
+  --           jump_prev = "[[",
+  --           jump_next = "]]",
+  --           accept = "<CR>",
+  --           refresh = "gr",
+  --           open = "<M-CR>",
+  --         },
+  --         layout = {
+  --           position = "bottom", -- | top | left | right
+  --           ratio = 0.4,
+  --         },
+  --       },
+  --       suggestion = {
+  --         enabled = true,
+  --         auto_trigger = true,
+  --         debounce = 75,
+  --         keymap = {
+  --           accept = "<C-j>",
+  --           accept_word = false,
+  --           accept_line = false,
+  --           -- next = "<M-]>",
+  --           -- prev = "<M-[>",
+  --           dismiss = "<C-]>",
+  --         },
+  --       },
+  --       filetypes = {
+  --         yaml = false,
+  --         markdown = false,
+  --         help = false,
+  --         gitcommit = false,
+  --         gitrebase = false,
+  --         hgcommit = false,
+  --         svn = false,
+  --         cvs = false,
+  --         ["."] = false,
+  --       },
+  --       copilot_node_command = "node", -- Node.js version must be > 18.x
+  --       server_opts_overrides = {},
+  --     }
+  --   end,
+  -- },
+
+
+  -- Codeium in Neovim
+  {
+    "Exafunction/codeium.vim",
+    event = "BufEnter",
+    -- config = function()
+    --   -- vim.g.codeium_disable_bindings = 1
+    --   vim.g.codeium_enabled = true
+    --   vim.g.codeium_filetypes_disabled_by_default = true
+    --   vim.g.codeium_filetypes = {
+    --   }
+    -- end,
+
+    -- config = function ()
+    --     -- Change '<C-g>' here to any keycode you like.
+    --     vim.keymap.set('i', '<C-g>', function () return vim.fn['codeium#Accept']() end, { expr = true, silent = true })
+    --     vim.keymap.set('i', '<c-;>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true, silent = true })
+    --     vim.keymap.set('i', '<c-,>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true, silent = true })
+    --     vim.keymap.set('i', '<c-x>', function() return vim.fn['codeium#Clear']() end, { expr = true, silent = true })
+    --   end
+
+    -- Clear current suggestion 	codeium#Clear() 	<C-]>
+    -- Next suggestion 	codeium#CycleCompletions(1) 	<M-]>
+    -- Previous suggestion 	codeium#CycleCompletions(-1) 	<M-[>
+    -- Insert suggestion 	codeium#Accept() 	<Tab>
+    -- Manually trigger suggestion 	codeium#Complete() 	<M-Bslash>
+    -- Accept word from suggestion 	codeium#AcceptNextWord() 	<C-k>
+    -- Accept line from suggestion 	codeium#AcceptNextLine() 	<C-l>
+  },
+
+  -- Latex Compilation in Neovim
+  {
+    "lervag/vimtex",
+    lazy = false,
+    init = function()
+      vim.g.vimtex_view_method = "zathura"
+      -- vim.g.vimtex_view_method = "okular"
+      vim.g.vimtex_syntax_enabled = 1
+    end,
+  },
+
+  -- https://nvimdev.github.io/lspsaga/
+  {
+    'nvimdev/lspsaga.nvim',
+    lazy = false,
+    event = "LspAttach",
+    config = function()
+      require('lspsaga').setup({})
+    end,
     dependencies = {
-      {
-        -- snippet plugin
-        "L3MON4D3/LuaSnip",
-        dependencies = "rafamadriz/friendly-snippets",
-        opts = { history = true, updateevents = "TextChanged,TextChangedI" },
-        config = function(_, opts)
-          require("plugins.configs.others").luasnip(opts)
-        end,
-      },
+      'nvim-treesitter/nvim-treesitter', -- optional
+      'nvim-tree/nvim-web-devicons',     -- optional
+    }
+  },
 
-      -- autopairing of (){}[] etc
-      {
-        "windwp/nvim-autopairs",
-        opts = {
-          fast_wrap = {},
-          disable_filetype = { "TelescopePrompt", "vim" },
-        },
-        config = function(_, opts)
-          require("nvim-autopairs").setup(opts)
 
-          -- setup cmp for autopairs
-          local cmp_autopairs = require "nvim-autopairs.completion.cmp"
-          require("cmp").event:on("confirm_done", cmp_autopairs.on_confirm_done())
-        end,
-      },
-
-      -- cmp sources plugins
-      {
-        "saadparwaiz1/cmp_luasnip",
-        "hrsh7th/cmp-nvim-lua",
-        "hrsh7th/cmp-nvim-lsp",
-        "hrsh7th/cmp-buffer",
-        "hrsh7th/cmp-path",
-      },
-    },
-    opts = function()
-      return require "plugins.configs.cmp"
-    end,
-    config = function(_, opts)
-      require("cmp").setup(opts)
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    commit = "29be0919b91fb59eca9e90690d76014233392bef",
+    opts = {},
+    config = function()
+      require "configs.ibl"
     end,
   },
 
+
+  -- Alt Dashboard
+  -- {
+  --     "nvimdev/dashboard-nvim",
+  --     event = "VimEnter",
+  --     config = function()
+  --       require("dashboard").setup {
+  --         -- config
+  --       }
+  --       require "configs.dashboard-nvim"
+  --     end,
+  --     dependencies = { { "nvim-tree/nvim-web-devicons" } },
+  --   },
+
+  -- Wakatime for Neovim
+
+  { "wakatime/vim-wakatime", lazy = false },
   {
-    "numToStr/Comment.nvim",
+    "folke/zen-mode.nvim",
+    cmd = "ZenMode",
+    config = function()
+      require "configs.zen-mode"
+    end,
+  },
+
+  -- MultiCursors for Neovim
+  {
+    "smoka7/multicursors.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "smoka7/hydra.nvim",
+    },
+    opts = {},
+    cmd = { "MCstart", "MCvisual", "MCclear", "MCpattern", "MCvisualPattern", "MCunderCursor" },
     keys = {
-      { "gcc", mode = "n", desc = "Comment toggle current line" },
-      { "gc", mode = { "n", "o" }, desc = "Comment toggle linewise" },
-      { "gc", mode = "x", desc = "Comment toggle linewise (visual)" },
-      { "gbc", mode = "n", desc = "Comment toggle current block" },
-      { "gb", mode = { "n", "o" }, desc = "Comment toggle blockwise" },
-      { "gb", mode = "x", desc = "Comment toggle blockwise (visual)" },
+      {
+        mode = { "v", "n" },
+        "<Leader>m",
+        "<cmd>MCstart<cr>",
+        desc = "Create a selection for selected text or word under the cursor",
+      },
     },
-    init = function()
-      require("core.utils").load_mappings "comment"
-    end,
-    config = function(_, opts)
-      require("Comment").setup(opts)
-    end,
   },
 
-  -- file managing , picker etc
-  {
-    "nvim-tree/nvim-tree.lua",
-    cmd = { "NvimTreeToggle", "NvimTreeFocus" },
-    init = function()
-      require("core.utils").load_mappings "nvimtree"
-    end,
-    opts = function()
-      return require "plugins.configs.nvimtree"
-    end,
-    config = function(_, opts)
-      dofile(vim.g.base46_cache .. "nvimtree")
-      require("nvim-tree").setup(opts)
-    end,
-  },
+  -- Ollama in Neovim
 
-  {
-    "nvim-telescope/telescope.nvim",
-    dependencies = { "nvim-treesitter/nvim-treesitter" },
-    cmd = "Telescope",
-    init = function()
-      require("core.utils").load_mappings "telescope"
-    end,
-    opts = function()
-      return require "plugins.configs.telescope"
-    end,
-    config = function(_, opts)
-      dofile(vim.g.base46_cache .. "telescope")
-      local telescope = require "telescope"
-      telescope.setup(opts)
-
-      -- load extensions
-      for _, ext in ipairs(opts.extensions_list) do
-        telescope.load_extension(ext)
-      end
-    end,
-  },
-
-  -- Only load whichkey after all the gui
-  {
-    "folke/which-key.nvim",
-    keys = { "<leader>", "<c-r>", "<c-w>", '"', "'", "`", "c", "v", "g" },
-    init = function()
-      require("core.utils").load_mappings "whichkey"
-    end,
-    cmd = "WhichKey",
-    config = function(_, opts)
-      dofile(vim.g.base46_cache .. "whichkey")
-      require("which-key").setup(opts)
-    end,
-  },
+  -- {
+  --    "David-Kunz/gen.nvim",
+  --    lazy = false,
+  --    opts = {
+  --      model = "mistral", -- The default model to use.
+  --      host = "localhost", -- The host running the Ollama service.
+  --      port = "11434", -- The port on which the Ollama service is listening.
+  --      display_mode = "float", -- The display mode. Can be "float" or "split".
+  --      show_prompt = false, -- Shows the Prompt submitted to Ollama.
+  --      show_model = false, -- Displays which model you are using at the beginning of your chat session.
+  --      no_auto_close = false, -- Never closes the window automatically.
+  --      init = function(options)
+  --        pcall(io.popen, "ollama serve > /dev/null 2>&1 &")
+  --      end,
+  --      -- Function to initialize Ollama
+  --      command = function(options)
+  --        return "curl --silent --no-buffer -X POST http://"
+  --          .. options.host
+  --          .. ":"
+  --          .. options.port
+  --          .. "/api/chat -d $body"
+  --      end,
+  --      -- The command for the Ollama service. You can use placeholders $prompt, $model and $body (shellescaped).
+  --      -- This can also be a command string.
+  --      -- The executed command must return a JSON object with { response, context }
+  --      -- (context property is optional).
+  --      -- list_models = '<omitted lua function>', -- Retrieves a list of model names
+  --      debug = false, -- Prints errors and the command which is run.
+  --    },
+  --    -- require('gen').select_model()
+  --  },
 }
-
-local config = require("core.utils").load_config()
-
-if #config.plugins > 0 then
-  table.insert(default_plugins, { import = config.plugins })
-end
-
-require("lazy").setup(default_plugins, config.lazy_nvim)
