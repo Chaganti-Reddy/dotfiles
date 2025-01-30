35fba8b1e7ec521d22b3659df507fb39a426dd7b
Chaganti-Reddy <chagantivenkataramireddy4@gmail.com>
2025-01-30 18:41:04 +0530

    Update chadrc.lua

--- nvim/.config/nvim/lua/chadrc.lua
+++ nvim/.config/nvim/lua/chadrc.lua
@@ -21,6 +21,49 @@ M.base46 = {
   theme_toggle = { "chadwal", "nightowl" },
 }
 
+-- M.nvdash = {
+--   load_on_startup = true,
+--   header = {
+--     "                            ",
+--     "     ▄▄         ▄ ▄▄▄▄▄▄▄   ",
+--     "   ▄▀███▄     ▄██ █████▀    ",
+--     "   ██▄▀███▄   ███           ",
+--     "   ███  ▀███▄ ███           ",
+--     "   ███    ▀██ ███           ",
+--     "   ███      ▀ ███           ",
+--     "   ▀██ █████▄▀█▀▄██████▄    ",
+--     "     ▀ ▀▀▀▀▀▀▀ ▀▀▀▀▀▀▀▀▀▀   ",
+--     "                            ",
+--     "     Powered By  eovim    ",
+--     "                            ",
+--   },
+--
+--   buttons = {
+--     { txt = "  Find File", keys = "ff", cmd = "Telescope find_files" },
+--     { txt = "  Recent Files", keys = "fo", cmd = "Telescope oldfiles" },
+--     { txt = "󰈭  Find Word", keys = "fw", cmd = "Telescope live_grep" },
+--     { txt = "󱥚  Themes", keys = "th", cmd = ":lua require('nvchad.themes').open()" },
+--     { txt = "  Mappings", keys = "ch", cmd = "NvCheatsheet" },
+--     { txt = "  Quit NVIM", keys = "qq", cmd = ":qa" },
+--
+--     { txt = "─", hl = "NvDashFooter", no_gap = true, rep = true },
+--
+--     {
+--       txt = function()
+--         local stats = require("lazy").stats()
+--         local ms = math.floor(stats.startuptime) .. " ms"
+--         return "  Loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms
+--       end,
+--       hl = "NvDashFooter",
+--       no_gap = true,
+--     },
+--
+--     { txt = "─", hl = "NvDashFooter", no_gap = true, rep = true },
+--   },
+--
+-- }
+--
+
 M.nvdash = {
   load_on_startup = true,
   header = {
@@ -41,6 +84,7 @@ M.nvdash = {
   buttons = {
     { txt = "  Find File", keys = "ff", cmd = "Telescope find_files" },
     { txt = "  Recent Files", keys = "fo", cmd = "Telescope oldfiles" },
+    { txt = "  Favourites", keys = "fd", cmd = ":lua QuickAccessMenu()" },
     { txt = "󰈭  Find Word", keys = "fw", cmd = "Telescope live_grep" },
     { txt = "󱥚  Themes", keys = "th", cmd = ":lua require('nvchad.themes').open()" },
     { txt = "  Mappings", keys = "ch", cmd = "NvCheatsheet" },
@@ -60,8 +104,50 @@ M.nvdash = {
 
     { txt = "─", hl = "NvDashFooter", no_gap = true, rep = true },
   },
-
 }
+
+-- Lua Function for Quick Access Menu
+function QuickAccessMenu()
+  local pickers = require("telescope.pickers")
+  local finders = require("telescope.finders")
+  local conf = require("telescope.config").values
+  local actions = require("telescope.actions")
+  local action_state = require("telescope.actions.state")
+
+  pickers.new({}, {
+    prompt_title = "Quick Access",
+    finder = finders.new_table({
+      results = {
+        { "Dotfiles", "~/dotfiles" },
+        { "Nvim Config", "~/.config/nvim/lua/" },
+        { "Portfolio", "/mnt/Karna/Git/portfolio/" },
+      },
+      entry_maker = function(entry)
+        return {
+          value = entry[2],
+          display = entry[1],
+          ordinal = entry[1],
+        }
+      end,
+    }),
+    sorter = conf.generic_sorter({}),
+    attach_mappings = function(prompt_bufnr, map)
+      local function open_dir()
+        local selection = action_state.get_selected_entry()
+        actions.close(prompt_bufnr)
+        vim.cmd("cd " .. selection.value)
+        vim.cmd("Telescope find_files")
+      end
+
+      map("i", "<CR>", open_dir)
+      map("n", "<CR>", open_dir)
+      return true
+    end,
+  }):find()
+end
+
+
+
 M.ui = {
   tabufline = {
     enabled = true,
