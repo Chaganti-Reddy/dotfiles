aebca6958361fe68a3df6aa8ea006a53e00de980
Chaganti-Reddy <chagantivenkataramireddy4@gmail.com>
2025-03-07 23:05:00 +0530

    create install-fonts.sh

---
+++ install-fonts.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Color definitions
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+CYAN='\033[0;36m'
+RESET='\033[0m'
+
+echo -e "${CYAN}Setting up Fonts...${RESET}" && sleep 1
+
+# Check if the fonts directory exists
+if [ -d "$HOME/.local/share/fonts/my-fonts-main" ]; then
+  echo -e "${GREEN}Fonts directory already exists. Skipping installation...${RESET}" && sleep 1
+else
+  # Create fonts directory if it doesn't exist
+  echo -e "${YELLOW}Creating fonts directory...${RESET}"
+  mkdir -p ~/.local/share/fonts
+
+  # Download the zip file
+  echo -e "${YELLOW}Downloading fonts...${RESET}"
+  curl -L -o ~/.local/share/fonts/my-fonts.zip https://github.com/Chaganti-Reddy/my-fonts/archive/main.zip
+
+  # Extract the fonts
+  echo -e "${YELLOW}Extracting fonts...${RESET}"
+  unzip -q ~/.local/share/fonts/my-fonts.zip -d ~/.local/share/fonts
+
+  # Clean up the downloaded zip file
+  echo -e "${YELLOW}Cleaning up...${RESET}"
+  rm ~/.local/share/fonts/my-fonts.zip
+
+  echo -e "${GREEN}Fonts have been installed successfully.${RESET}" && sleep 2
+fi
