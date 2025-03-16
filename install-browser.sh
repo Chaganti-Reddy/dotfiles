b56c8bf2c17aad097dc3e42f6f4463a2b0ec8157
Chaganti-Reddy <chagantivenkataramireddy4@gmail.com>
2025-03-16 17:41:40 +0530

    update install-browser.sh

--- install-browser.sh
+++ install-browser.sh
@@ -32,75 +32,76 @@ install_browser() {
 
   # Read user input (browser choices)
   read -r choices
-  choices=${choices:-6}  # Default to installing Zen-Browser if no input is provided 
+  choices=${choices:-6}  # Default to installing Zen-Browser if no input is provided
 
   # Process the selected options
   for choice in $choices; do
     case $choice in
       1)
-        if is_installed "zen-browser-bin"; then
-          echo -e "${MAGENTA}Zen-Browser is already installed.${RESET}"
-        else
-          echo -e "${CYAN}Installing Zen-Browser...${RESET}"
-          paru -S --noconfirm --needed zen-browser-bin
-          sudo npm install -g nativefier
-          pip install --index-url https://test.pypi.org/simple/ pywalfox==2.8.0rc1
-          pywalfox install
-          # Video Download Helper
-          curl -sSLf https://github.com/aclap-dev/vdhcoapp/releases/latest/download/install.sh | bash
-        fi
-        ;;
+	if is_installed "zen-browser-bin"; then
+	  echo -e "${MAGENTA}Zen-Browser is already installed.${RESET}"
+	else
+	  echo -e "${CYAN}Installing Zen-Browser...${RESET}"
+	  paru -S --noconfirm --needed zen-browser-bin
+	  sudo npm install -g nativefier
+	  pip install --index-url https://test.pypi.org/simple/ pywalfox==2.8.0rc1
+	  pywalfox install
+	  # Video Download Helper
+	  curl -sSLf https://github.com/aclap-dev/vdhcoapp/releases/latest/download/install.sh | bash
+	fi
+	;;
       2)
-        if is_installed "firefox"; then
-          echo -e "${MAGENTA}Firefox is already installed.${RESET}"
-        else
-          echo -e "${CYAN}Installing Firefox...${RESET}"
-          sudo pacman -S --noconfirm firefox
-          pip install --index-url https://test.pypi.org/simple/ pywalfox==2.8.0rc1
-          pywalfox install
-          curl -sSLf https://github.com/aclap-dev/vdhcoapp/releases/latest/download/install.sh | bash
-        fi
-        ;;
+	if is_installed "firefox"; then
+	  echo -e "${MAGENTA}Firefox is already installed.${RESET}"
+	else
+	  echo -e "${CYAN}Installing Firefox...${RESET}"
+	  sudo pacman -S --noconfirm firefox
+	  pip install --index-url https://test.pypi.org/simple/ pywalfox==2.8.0rc1
+	  pywalfox install
+	  curl -sSLf https://github.com/aclap-dev/vdhcoapp/releases/latest/download/install.sh | bash
+	fi
+	;;
       3)
-        if is_installed "chromium"; then
-          echo -e "${MAGENTA}Chromium is already installed.${RESET}"
-        else
-          echo -e "${CYAN}Installing Chromium...${RESET}"
-          sudo pacman -S --noconfirm chromium
-        fi
-        ;;
+	if is_installed "chromium"; then
+	  echo -e "${MAGENTA}Chromium is already installed.${RESET}"
+	else
+	  echo -e "${CYAN}Installing Chromium...${RESET}"
+	  sudo pacman -S --noconfirm chromium
+	fi
+	;;
       4)
-        if is_installed "vivaldi"; then
-          echo -e "${MAGENTA}Vivaldi is already installed.${RESET}"
-        else
-          echo -e "${CYAN}Installing Vivaldi...${RESET}"
-          paru -S --noconfirm vivaldi
-        fi
-        ;;
+	if is_installed "vivaldi"; then
+	  echo -e "${MAGENTA}Vivaldi is already installed.${RESET}"
+	else
+	  echo -e "${CYAN}Installing Vivaldi...${RESET}"
+	  paru -S --noconfirm vivaldi
+	fi
+	;;
       5)
-        if is_installed "qutebrowser"; then
-          echo -e "${MAGENTA}qutebrowser is already installed.${RESET}"
-        else
-          echo -e "${CYAN}Installing qutebrowser...${RESET}"
-          sudo pacman -S --noconfirm qutebrowser
-        fi
-        ;;
+	if is_installed "qutebrowser"; then
+	  echo -e "${MAGENTA}qutebrowser is already installed.${RESET}"
+	else
+	  echo -e "${CYAN}Installing qutebrowser...${RESET}"
+	  sudo pacman -S --noconfirm qutebrowser
+	fi
+	;;
       6)
-        if is_installed "brave-bin"; then
-          echo -e "${MAGENTA}Brave is already installed.${RESET}"
-        else
-          echo -e "${CYAN}Installing Brave...${RESET}"
-          paru -S --noconfirm brave-bin
-        fi
-        ;;
+	if is_installed "brave-bin"; then
+	  echo -e "${MAGENTA}Brave is already installed.${RESET}"
+	else
+	  echo -e "${CYAN}Installing Brave...${RESET}"
+	  paru -S --noconfirm brave-bin
+	  sudo npm install -g nativefier
+	fi
+	;;
       100)
-        clear
-        echo -e "${GREEN}Skipping all installations.${RESET}" && sleep 2
-        return
-        ;;
+	clear
+	echo -e "${GREEN}Skipping all installations.${RESET}" && sleep 2
+	return
+	;;
       *)
-        echo -e "${RED}Invalid choice: $choice${RESET}"
-        ;;
+	echo -e "${RED}Invalid choice: $choice${RESET}"
+	;;
     esac
     echo -e "${GREEN}Selected browsers have been installed.${RESET}" && sleep 2
   done
