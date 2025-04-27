6de37e395bc49e35f342235fba2f9d7217f0b37b
Chaganti-Reddy <chagantivenkataramireddy4@gmail.com>
2025-04-27 19:14:06 +0530

    update nvim/.config/nvim/lua/configs/lazy.lua

--- nvim/.config/nvim/lua/configs/lazy.lua
+++ nvim/.config/nvim/lua/configs/lazy.lua
@@ -2,12 +2,35 @@ return {
   defaults = { lazy = true },
   install = { colorscheme = { "nvchad" } },
 
+  change_detection = { notify = false },
+
+  -- ui = {
+  --   icons = {
+  --     ft = "",
+  --     lazy = "󰂠 ",
+  --     loaded = "",
+  --     not_loaded = "",
+  --   },
+  -- },
+
+
   ui = {
-    icons = {
-      ft = "",
-      lazy = "󰂠 ",
-      loaded = "",
-      not_loaded = "",
+    -- If you have a Nerd Font, set icons to an empty table which will use the
+    -- default lazy.nvim defined Nerd Font icons otherwise define a unicode icons table
+    icons = vim.g.have_nerd_font and {} or {
+      cmd = '⌘',
+      config = '🛠',
+      event = '📅',
+      ft = '📂',
+      init = '⚙',
+      keys = '🗝',
+      plugin = '🔌',
+      runtime = '💻',
+      require = '🌙',
+      source = '📄',
+      start = '🚀',
+      task = '📌',
+      lazy = '💤 ',
     },
   },
 
