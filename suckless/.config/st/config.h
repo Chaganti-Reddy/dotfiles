75a6f95ff40a9f16d28d2fd8ccb86f6a864849f3
Chaganti-Reddy <chagantivenkataramireddy4@gmail.com>
2025-05-04 23:02:59 +0530

    update suckless/.config/st/config.h

--- suckless/.config/st/config.h
+++ suckless/.config/st/config.h
@@ -6,7 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 // static char *font = "JetBrains Mono:pixelsize=16:antialias=true:autohint=true";
-static char *font = "Aporetic Sans Mono:pixelsize=17:antialias=true:autohint=true";
+static char *font = "Iosevka Nerd Font:weight=bold:pixelsize=17:antialias=true:autohint=true";
 static char *font2[] = { "JoyPixels:pixelsize=17:antialias=true:autohint=true" };
 static int borderpx = 15;
 
