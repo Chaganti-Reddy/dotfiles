f5139e15cb74d65f0558f34185252f69fc726fb1
Chaganti-Reddy <chagantivenkataramireddy4@gmail.com>
2025-05-08 22:25:18 +0530

    update DWMScripts/.dwm/startdwm.sh

--- DWMScripts/.dwm/startdwm.sh
+++ DWMScripts/.dwm/startdwm.sh
@@ -5,17 +5,11 @@
 # setxkbmap us,ru -option 'grp:caps_toggle'
 
 # Start dwmblocks in background
-dwmblocks &
+# dwmblocks &
 
-# Loop to restart dwm only on error/quit({1})
 while true; do
     # Log stderr to a file
     dwm 2>~/.dwm.log
 
-    # Get dwm's exit status
-    status=$?
-
-    # Exit loop if dwm quit with status 1
-    [ $status -eq 1 ] && break
 done
 
