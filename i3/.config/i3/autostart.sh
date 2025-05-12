8708bd0ef9d38a6b92ba1387037e8a1092bf9ef0
Chaganti-Reddy <chagantivenkataramireddy4@gmail.com>
2025-05-12 21:52:50 +0530

    update i3/.config/i3/autostart.sh

--- i3/.config/i3/autostart.sh
+++ i3/.config/i3/autostart.sh
@@ -5,10 +5,10 @@ dunst -conf ~/.config/dunst/dunstrc_xorg &
 ~/.config/i3/redshift.sh &
 # nm-applet &
 # xfce4-clipman &
-greenclip daemon
+greenclip daemon &
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 mpd &
 # picom &
 # Run ollama serve in the background silently.
 ollama serve > /dev/null 2>&1 &
-~/.config/i3/random_wall &
+~/.config/i3/random_wall > /tmp/random_wall.log &
