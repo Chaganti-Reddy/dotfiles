ecd3ba7764d421778e0b30039ccfafbb934dd22c
Chaganti-Reddy <chagantivenkataramireddy4@gmail.com>
2025-06-02 00:16:48 +0530

    update dwm/.dwm/xidlehook.sh

--- dwm/.dwm/xidlehook.sh
+++ dwm/.dwm/xidlehook.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
+# Check and kill any existing xidlehook process
+if pidof xidlehook >/dev/null; then
+  killall xidlehook
+fi 
+
+sleep 0.5
+
+# Start xidlehook fresh
+# dunstify -u low --replace=10034 "xidlehook" "Started"
+
 xidlehook \
   --not-when-fullscreen \
   --not-when-audio \
   --timer 60 \
-    'dunstify -u low --replace=1669 "Idle" "Dimming screen to 30%"; brightnessctl set 30%' \
+    'brightnessctl set 30%' \
     'brightnessctl set 100%' \
   --timer 600 \
     'dunstify -u low --replace=1669 "Locking" "Locking the screen"; brightnessctl set 100%; betterlockscreen -l dim' '' \
@@ -13,3 +23,4 @@ xidlehook \
   --timer 900 \
     'dunstify -u low --replace=1669 "Suspending" "System will now suspend"; systemctl suspend' '' \
   --socket "/tmp/xidlehook.sock"
+
