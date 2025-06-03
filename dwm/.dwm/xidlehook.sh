570d91f452966fb901dbb988e214381e05e4055b
Chaganti-Reddy <chagantivenkataramireddy4@gmail.com>
2025-06-03 12:28:32 +0530

    update dwm/.dwm/xidlehook.sh

--- dwm/.dwm/xidlehook.sh
+++ dwm/.dwm/xidlehook.sh
@@ -16,10 +16,10 @@ xidlehook \
   --timer 60 \
     'brightnessctl set 30%' \
     'brightnessctl set 100%' \
+    --timer 300 \
+    'dunstify -u low --replace=1669 "Display" "Turning off screen"; xset dpms force off' '' \
   --timer 600 \
     'dunstify -u low --replace=1669 "Locking" "Locking the screen"; brightnessctl set 100%; betterlockscreen -l dim' '' \
-  --timer 300 \
-    'dunstify -u low --replace=1669 "Display" "Turning off screen"; xset dpms force off' '' \
   --timer 900 \
     'dunstify -u low --replace=1669 "Suspending" "System will now suspend"; systemctl suspend' '' \
   --socket "/tmp/xidlehook.sock"
