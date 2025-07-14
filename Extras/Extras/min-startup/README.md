7c455496e206cee6aecb6ce1a494b70fe10a2584
Chaganti-Reddy <chagantivenkataramireddy4@gmail.com>
2025-07-14 20:54:14 +0530

    create Extras/Extras/min-startup/

---
+++ Extras/Extras/min-startup/README.md
@@ -0,0 +1,11 @@
+![screenshot](./git-assets/ss.png)
+
+# Custom Browser Start Page
+
+Just a simple start page to replace the default one.
+
+Browser Integration:
+
+In order for this to work, your browser must support having custom home pages. For the Brave browser, this is done through enabling "Show home button" and the providing the absolute path to the html file, prefixed with "file://"
+
+Example: `file:///Users/username/dev/min-startup/index.html`
---
+++ Extras/Extras/min-startup/index.js
@@ -0,0 +1,49 @@
+const links = {
+  a: "https://github.com",
+  s: "https://youtube.com",
+  d: "https://reddit.com",
+  f: {
+    href: "https://calendar.google.com",
+    alias: "calendar",
+  },
+  g: "https://tldraw.com",
+};
+
+/**
+ * @param {string} key
+ * @returns {string}
+ * */
+const key_to_letter = (key) => {
+  return key[key.length - 1].toLowerCase().toString();
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  window.addEventListener("keypress", (e) => {
+    const link = links[key_to_letter(e.code)];
+    if (link) {
+      window.location.href = link;
+    }
+  });
+
+  const list_element = document.getElementById("list");
+  for (const [key, value] of Object.entries(links)) {
+    let list_item = document.createElement("li");
+    let list_link = document.createElement("a");
+
+    let display_title = "nan";
+    if (typeof value === "object") {
+      display_title = value.alias;
+    } else {
+      display_title = value
+        .replace("https://", "")
+        .replace(/\.[a-zA-Z0-9]+$/, "");
+    }
+
+    list_link.innerText = `[${key}] ${display_title}`;
+    list_link.value = value;
+    list_link.href = value;
+
+    list_item.appendChild(list_link);
+    list_element.appendChild(list_item);
+  }
+});
