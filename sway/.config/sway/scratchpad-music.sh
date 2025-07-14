bd520c319f5e7f9eeb7453806c75a6e8e583f973
Chaganti-Reddy <chagantivenkataramireddy4@gmail.com>
2025-07-14 20:54:13 +0530

    delete sway/.config/sway/scratchpad-music.sh

--- sway/.config/sway/scratchpad-music.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-TITLE="scratchpad-music"
-CLASS="Music"
-
-if swaymsg -t get_tree | grep -q "\"name\": \"$TITLE\""; then
-  swaymsg [title="$TITLE"] scratchpad show
-else
-  st -c "$CLASS" -e ~/.ncmpcpp/scripts/ncmpcpp-art &
-fi
