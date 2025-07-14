adc2e9b12792d208e7de6331f47da99fa785a317
Chaganti-Reddy <chagantivenkataramireddy4@gmail.com>
2025-07-14 20:54:13 +0530

    delete sway/.config/sway/scratchpad-calc.sh

--- sway/.config/sway/scratchpad-calc.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-TITLE="scratchpad-calc"
-CLASS="qalculate-gtk"
-
-if swaymsg -t get_tree | grep -q "\"name\": \"$TITLE\""; then
-  swaymsg [title="$TITLE"] scratchpad show
-else
-  qalculate-gtk --class "$CLASS" --title "$TITLE" &
-fi
