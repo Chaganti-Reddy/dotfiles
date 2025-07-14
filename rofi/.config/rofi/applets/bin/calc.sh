abfd2f69a330ad1eece529df7896acb55dbb64a2
Chaganti-Reddy <chagantivenkataramireddy4@gmail.com>
2025-07-14 20:54:12 +0530

    delete rofi/.config/rofi/applets/bin/calc.sh

--- rofi/.config/rofi/applets/bin/calc.sh
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env bash
-
-dir="$HOME/.config/rofi/launchers/type-1"
-theme='style-5'
-
-## Run
-rofi \
-    -show calc -modi calc -no-show-match -no-sort  \
-    -theme ${dir}/${theme}.rasi
