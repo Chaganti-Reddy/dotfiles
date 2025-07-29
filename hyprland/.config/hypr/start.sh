d97a1cda9f88bf7f2e15af384d8edc687857b3ba
Chaganti-Reddy <chagantivenkataramireddy4@gmail.com>
2025-07-29 23:26:11 +0530

    update hyprland/.config/hypr/start.sh

--- hyprland/.config/hypr/start.sh
+++ hyprland/.config/hypr/start.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-export SSH_AUTH_SOCK
-export GPG_AGENT_INFO
-export GNOME_KEYRING_CONTROL
-export GNOME_KEYRING_PID
-
 # sh ~/.config/scripts/redshift.sh &
 sh ~/.config/scripts/hyprstyle &
 
 # Run ollama serve in the background silently.
-ollama serve > /dev/null 2>&1 &
+# ollama serve > /dev/null 2>&1 &
