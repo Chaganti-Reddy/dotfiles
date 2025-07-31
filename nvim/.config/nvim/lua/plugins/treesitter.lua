fe50421144a88f63c0923c28febf0057ec628bc4
Chaganti-Reddy <chagantivenkataramireddy4@gmail.com>
2025-07-31 16:27:43 +0530

    update nvim/.config/nvim/lua/plugins/treesitter.lua

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,48 +1,48 @@
 return {
-    {
-        "nvim-treesitter/nvim-treesitter",
-        build = ":TSUpdate",
-        config = function()
-            local configs = require("nvim-treesitter.configs")
-            configs.setup({
-                -- enable syntax highlighting
-                highlight = {
-                    enable = true,
-                },
-                -- enable indentation
-                indent = { enable = true },
-                -- enable autotagging (w/ nvim-ts-autotag plugin)
-                autotag = { enable = true },
-                -- ensure these language parsers are installed
-                ensure_installed = {
-                    "json",
-                    "javascript",
-                    "java",
-                    "javadoc",
-                    "query",
-                    "python",
-                    "typescript",
-                    "tsx",
-                    "php",
-                    "yaml",
-                    "html",
-                    "css",
-                    "markdown",
-                    "markdown_inline",
-                    "bash",
-                    "lua",
-                    "vim",
-                    "vimdoc",
-                    "c",
-                    "cpp",
-                    "dockerfile",
-                    "gitignore",
-                    "astro",
-                    "hyprlang"
-                },
-                -- auto install above language parsers
-                auto_install = false,
-            })
-        end
-    }
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			local configs = require("nvim-treesitter.configs")
+			configs.setup({
+				-- enable syntax highlighting
+				highlight = {
+					enable = true,
+				},
+				-- enable indentation
+				indent = { enable = true },
+				-- enable autotagging (w/ nvim-ts-autotag plugin)
+				autotag = { enable = true },
+				-- ensure these language parsers are installed
+				ensure_installed = {
+					"json",
+					"javascript",
+					"java",
+					"javadoc",
+					"query",
+					"python",
+					"typescript",
+					"tsx",
+					"php",
+					"yaml",
+					"html",
+					"css",
+					"markdown",
+					"markdown_inline",
+					"bash",
+					"lua",
+					"vim",
+					"vimdoc",
+					"c",
+					"cpp",
+					"dockerfile",
+					"gitignore",
+					"astro",
+					"hyprlang",
+				},
+				-- auto install above language parsers
+				auto_install = true,
+			})
+		end,
+	},
 }
