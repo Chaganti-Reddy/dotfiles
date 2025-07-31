47977db4defed1795c4903d9529e61dd5d1df05a
Chaganti-Reddy <chagantivenkataramireddy4@gmail.com>
2025-07-31 11:43:04 +0530

    update nvim/.config/nvim/lua/config/options.lua

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,9 +1,16 @@
 -- OPTIONS
 local set = vim.opt
+local opt = vim.opt
+local o = vim.o
+local g = vim.g
 
 --line nums
-set.relativenumber = true
-set.number = true
+o.number = true
+o.relativenumber = true
+o.numberwidth = 2
+o.ruler = false
+o.laststatus = 3
+o.showmode = false
 
 -- indentation and tabs
 set.tabstop = 4
@@ -12,13 +19,21 @@ set.autoindent = true
 set.expandtab = true
 
 -- search settings
-set.ignorecase = true
-set.smartcase = true
+opt.fillchars = { eob = " " }
+o.ignorecase = true
+o.smartcase = true
+o.mouse = "a"
 
 -- appearance
 set.termguicolors = true
 set.background = "dark"
 set.signcolumn = "yes"
+o.splitbelow = true
+o.splitright = true
+o.timeoutlen = 400
+
+-- disable nvim intro
+opt.shortmess:append("sI")
 
 -- cursor line
 -- set.cursorline = true
@@ -28,6 +43,8 @@ set.signcolumn = "yes"
 
 -- clipboard
 set.clipboard:append("unnamedplus")
+o.cursorline = true
+o.cursorlineopt = "number"
 
 -- backspace
 set.backspace = "indent,eol,start"
@@ -53,3 +70,19 @@ set.incsearch = true
 
 -- faster cursor hold
 set.updatetime = 50
+
+-- go to previous/next line with h,l,left arrow and right arrow
+-- when cursor reaches end/beginning of line
+opt.whichwrap:append("<>[]hl")
+
+-- disable some default providers
+g.loaded_node_provider = 0
+g.loaded_python3_provider = 0
+g.loaded_perl_provider = 0
+g.loaded_ruby_provider = 0
+
+-- add binaries installed by mason.nvim to path
+local is_windows = vim.fn.has("win32") ~= 0
+local sep = is_windows and "\\" or "/"
+local delim = is_windows and ";" or ":"
+vim.env.PATH = table.concat({ vim.fn.stdpath("data"), "mason", "bin" }, sep) .. delim .. vim.env.PATH
