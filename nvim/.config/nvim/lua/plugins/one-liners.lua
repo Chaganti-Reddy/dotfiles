a420fc864b0950b15b329f13d099762f372f7009
Chaganti-Reddy <chagantivenkataramireddy4@gmail.com>
2025-07-31 11:26:33 +0530

    update nvim/.config/nvim/lua/plugins/one-liners.lua

--- nvim/.config/nvim/lua/plugins/one-liners.lua
+++ nvim/.config/nvim/lua/plugins/one-liners.lua
@@ -23,11 +23,84 @@ return {
     },
     {
         'MeanderingProgrammer/render-markdown.nvim',
-        dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
-        -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
-        -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
-        ---@module 'render-markdown'
-        ---@type render.md.UserConfig
+        dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' },
         opts = {},
+    },
+
+    {
+        "Exafunction/codeium.vim",
+        event = "BufEnter",
+    },
+    {
+        "stevearc/conform.nvim",
+        event = 'BufWritePre', -- uncomment for format on save
+        opts = require "config.conform",
+    },
+
+    {
+        "lervag/vimtex",
+        lazy = false,
+        init = function()
+        vim.g.vimtex_view_method = "zathura"
+        -- vim.g.vimtex_view_method = "okular"
+        vim.g.vimtex_syntax_enabled = 1
+        end,
+    },
+
+    -- https://nvimdev.github.io/lspsaga/
+    {
+        "nvimdev/lspsaga.nvim",
+        lazy = false,
+        event = "LspAttach",
+        config = function()
+        require("lspsaga").setup {}
+        end,
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter", -- optional
+            "nvim-tree/nvim-web-devicons", -- optional
+        },
+    },
+
+    {
+        "lukas-reineke/indent-blankline.nvim",
+        main = "ibl",
+        commit = "29be0919b91fb59eca9e90690d76014233392bef",
+        opts = {},
+        config = function()
+        require "config.ibl"
+        end,
+    },
+
+    { "wakatime/vim-wakatime", lazy = false },
+    {
+        "folke/zen-mode.nvim",
+        cmd = "ZenMode",
+        config = function()
+        require "config.zen-mode"
+        end,
+    },
+
+    {
+        "mg979/vim-visual-multi",
+        lazy = false,
+    },
+
+    {
+        -- Hints keybinds
+        'folke/which-key.nvim',
+        opts = {
+            -- win = {
+            --   border = {
+            --     { '┌', 'FloatBorder' },
+            --     { '─', 'FloatBorder' },
+            --     { '┐', 'FloatBorder' },
+            --     { '│', 'FloatBorder' },
+            --     { '┘', 'FloatBorder' },
+            --     { '─', 'FloatBorder' },
+            --     { '└', 'FloatBorder' },
+            --     { '│', 'FloatBorder' },
+            --   },
+            -- },
+        },
     },
 }
