4d25976fd8087d6ef1518f02ed387d8b7df75c25
Chaganti-Reddy <chagantivenkataramireddy4@gmail.com>
2025-07-31 11:32:11 +0530

    update nvim/.config/nvim/lua/config/keybinds.lua

--- nvim/.config/nvim/lua/config/keybinds.lua
+++ nvim/.config/nvim/lua/config/keybinds.lua
@@ -120,18 +120,6 @@ vim.keymap.set("n", "<leader>csv", "<cmd>VimtexStop<CR>", { desc = "VimTeX: Stop
 
 vim.keymap.set("n", "<leader>z", "<cmd>ZenMode<CR>", { desc = "Toggle Zen Mode" })
 
-local dap = require('dap')
-
--- Basic debugging keymaps
-vim.keymap.set('n', '<F5>', dap.continue, { desc = 'Debug: Start/Continue' })
-vim.keymap.set('n', '<F2>', dap.step_over, { desc = 'Debug: Step Over' })
-vim.keymap.set('n', '<F1>', dap.step_into, { desc = 'Debug: Step Into' })
-vim.keymap.set('n', '<F3>', dap.step_out, { desc = 'Debug: Step Out' })
-vim.keymap.set('n', '<leader>b', dap.toggle_breakpoint, { desc = 'Debug: Toggle Breakpoint' })
-vim.keymap.set('n', '<leader>B', function()
-dap.set_breakpoint(vim.fn.input('Breakpoint condition: '))
-end, { desc = 'Debug: Set Conditional Breakpoint' })
-
 vim.keymap.set("n", "<leader>fm", function()
 require("conform").format { lsp_fallback = true }
 end, { desc = "General format file" })
