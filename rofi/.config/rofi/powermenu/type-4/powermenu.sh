71794e5fee8d495ba07c209abd4e4c5c94d53282
Chaganti-Reddy <chagantivenkataramireddy4@gmail.com>
2025-08-02 10:13:27 +0530

    update rofi/.config/rofi/powermenu/type-4/powermenu.sh

--- rofi/.config/rofi/powermenu/type-4/powermenu.sh
+++ rofi/.config/rofi/powermenu/type-4/powermenu.sh
@@ -5,8 +5,8 @@ dir="$HOME/.config/rofi/powermenu/type-4"
 theme='style-5'
 
 # CMDs
-uptime="`uptime -p | sed -e 's/up //g'`"
-host=`hostname`
+uptime="$(uptime -p | sed -e 's/up //g')"
+host=$(hostname)
 
 # Options
 shutdown=''
@@ -19,80 +19,80 @@ no=''
 
 # Rofi CMD
 rofi_cmd() {
-	rofi -dmenu \
-		-p "Goodbye ${USER}" \
-		-mesg "Uptime: $uptime" \
-		-theme ${dir}/${theme}.rasi
+    rofi -dmenu \
+        -p "Goodbye ${USER}" \
+        -mesg "Uptime: $uptime" \
+        -theme ${dir}/${theme}.rasi
 }
 
 # Confirmation CMD
 confirm_cmd() {
-	rofi -dmenu \
-		-p 'Confirmation' \
-		-mesg 'Are you Sure?' \
-		-theme ${dir}/shared/confirm.rasi
+    rofi -dmenu \
+        -p 'Confirmation' \
+        -mesg 'Are you Sure?' \
+        -theme ${dir}/shared/confirm.rasi
 }
 
 # Ask for confirmation
 confirm_exit() {
-	echo -e "$yes\n$no" | confirm_cmd
+    echo -e "$yes\n$no" | confirm_cmd
 }
 
 # Pass variables to rofi dmenu
 run_rofi() {
-	echo -e "$lock\n$suspend\n$logout\n$reboot\n$shutdown" | rofi_cmd
+    echo -e "$lock\n$suspend\n$logout\n$reboot\n$shutdown" | rofi_cmd
 }
 
 # Execute Command
 run_cmd() {
-	selected="$(confirm_exit)"
-	if [[ "$selected" == "$yes" ]]; then
-		if [[ $1 == '--shutdown' ]]; then
-			systemctl poweroff
-		elif [[ $1 == '--reboot' ]]; then
-			systemctl reboot
-		elif [[ $1 == '--suspend' ]]; then
-			mpc -q pause
-			pamixer -m
-			systemctl suspend
-		elif [[ $1 == '--logout' ]]; then
-			if [[ "$DESKTOP_SESSION" == 'openbox' ]]; then
-				openbox --exit
-			elif [[ "$DESKTOP_SESSION" == 'bspwm' ]]; then
-				bspc quit
-			elif [[ "$DESKTOP_SESSION" == 'i3' ]]; then
-				i3-msg exit
-      elif [[ "$DESKTOP_SESSION" == 'hyprland' ]]; then 
-        hyprctl dispatch exit 0
-      elif [[ "$DESKTOP_SESSION" == 'sway' ]]; then
-        swaymsg exit 0
-      elif [[ "$DESKTOP_SESSION" == 'qtile' ]]; then
-        qtile cmd-obj -o cmd -f shutdown
-			fi
-		fi
-	else
-		exit 0
-	fi
+    selected="$(confirm_exit)"
+    if [[ "$selected" == "$yes" ]]; then
+        if [[ $1 == '--shutdown' ]]; then
+            systemctl poweroff
+        elif [[ $1 == '--reboot' ]]; then
+            systemctl reboot
+        elif [[ $1 == '--suspend' ]]; then
+            mpc -q pause
+            pamixer -m
+            systemctl suspend
+        elif [[ $1 == '--logout' ]]; then
+            if [[ "$DESKTOP_SESSION" == 'openbox' ]]; then
+                openbox --exit
+            elif [[ "$DESKTOP_SESSION" == 'bspwm' ]]; then
+                bspc quit
+            elif [[ "$DESKTOP_SESSION" == 'i3' ]]; then
+                i3-msg exit
+            elif [[ "$DESKTOP_SESSION" == 'hyprland' ]]; then
+                hyprctl dispatch exit 0
+            elif [[ "$DESKTOP_SESSION" == 'sway' ]]; then
+                swaymsg exit 0
+            elif [[ "$DESKTOP_SESSION" == 'qtile' ]]; then
+                qtile cmd-obj -o cmd -f shutdown
+            fi
+        fi
+    else
+        exit 0
+    fi
 }
 
 # Actions
 chosen="$(run_rofi)"
 case ${chosen} in
-    $shutdown)
-    echo "$(uptime -p) from $(uptime -s) till $(date '+%Y-%m-%d %H:%M:%S')...  Shutting Down" >> ~/dotfiles/Uptime.log
-		run_cmd --shutdown
-        ;;
-    $reboot)
-    echo "$(uptime -p) from $(uptime -s) till $(date '+%Y-%m-%d %H:%M:%S')...  Rebooting" >> ~/dotfiles/Uptime.log
-		run_cmd --reboot
-        ;;
-    $lock)
-	  hyprlock &	
-        ;;
-    $suspend)
-		run_cmd --suspend
-        ;;
-    $logout)
-		run_cmd --logout
-        ;;
+$shutdown)
+    echo "$(uptime -p) from $(uptime -s) till $(date '+%Y-%m-%d %H:%M:%S')...  Shutting Down" >>~/dotfiles/Uptime.log
+    run_cmd --shutdown
+    ;;
+$reboot)
+    echo "$(uptime -p) from $(uptime -s) till $(date '+%Y-%m-%d %H:%M:%S')...  Rebooting" >>~/dotfiles/Uptime.log
+    run_cmd --reboot
+    ;;
+$lock)
+    hyprlock &
+    ;;
+$suspend)
+    run_cmd --suspend
+    ;;
+$logout)
+    run_cmd --logout
+    ;;
 esac
