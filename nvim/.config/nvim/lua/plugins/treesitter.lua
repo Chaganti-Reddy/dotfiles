717ced8df9e9fa077f76b52ad221afee6fd1077c
Chaganti-Reddy <chagantivenkataramireddy4@gmail.com>
2026-01-03 17:05:29 +0530

    create nvim/.config/nvim/lua/plugins/treesitter.lua

---
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,48 @@
+return {
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			local configs = require("nvim-treesitter.config")
+			configs.setup({
+				-- enable syntax highlighting
+				highlight = {
+					enable = true,
+				},
+				-- enable indentation
+				indent = { enable = true },
+				-- enable autotagging (w/ nvim-ts-autotag plugin)
+				autotag = { enable = true },
+				-- ensure these language parsers are installed
+				ensure_installed = {
+					"json",
+					"javascript",
+					"java",
+					"javadoc",
+					"query",
+					"python",
+					"typescript",
+					"tsx",
+					"php",
+					"yaml",
+					"html",
+					"css",
+					"markdown",
+					"markdown_inline",
+					"bash",
+					"lua",
+					"vim",
+					"vimdoc",
+					"c",
+					"cpp",
+					"dockerfile",
+					"gitignore",
+					"astro",
+					"hyprlang",
+				},
+				-- auto install above language parsers
+				auto_install = true,
+			})
+		end,
+	},
+}
