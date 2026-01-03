dff3bf5686f81d3fb71893ba401c08893e4fad7d
Chaganti-Reddy <chagantivenkataramireddy4@gmail.com>
2026-01-03 17:05:29 +0530

    create nvim/.config/nvim/lua/plugins/neotree.lua

---
+++ nvim/.config/nvim/lua/plugins/neotree.lua
@@ -0,0 +1,37 @@
+return {
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+		"MunifTanjim/nui.nvim",
+		{
+			"s1n7ax/nvim-window-picker",
+			version = "2.*",
+			config = function()
+				require("window-picker").setup({
+					filter_rules = {
+						include_current_win = false,
+						autoselect_one = true,
+						-- filter using buffer options
+						bo = {
+							-- if the file type is one of following, the window will be ignored
+							filetype = { "neo-tree", "neo-tree-popup", "notify" },
+							-- if the buffer type is one of following, the window will be ignored
+							buftype = { "terminal", "quickfix" },
+						},
+					},
+				})
+			end,
+		},
+	},
+	config = function()
+		-- If you want icons for diagnostic errors, you'll need to define them somewhere:
+		vim.fn.sign_define("DiagnosticSignError", { text = " ", texthl = "DiagnosticSignError" })
+		vim.fn.sign_define("DiagnosticSignWarn", { text = " ", texthl = "DiagnosticSignWarn" })
+		vim.fn.sign_define("DiagnosticSignInfo", { text = " ", texthl = "DiagnosticSignInfo" })
+		vim.fn.sign_define("DiagnosticSignHint", { text = "󰌵", texthl = "DiagnosticSignHint" })
+
+		vim.keymap.set("n", "<space>e", "<cmd>Neotree toggle<cr>", { desc = "file explorer tree toggle" })
+	end,
+}
