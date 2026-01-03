6b4f12fe8a1f2501b7623d88d8793300a384d187
Chaganti-Reddy <chagantivenkataramireddy4@gmail.com>
2026-01-03 17:05:25 +0530

    update nvim/.config/nvim/init.lua

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,2 +1,13 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.options")
+require("config.keybinds")
 require("config.lazy")
+
+vim.api.nvim_create_autocmd("BufReadPost", {
+	callback = function()
+		local mark = vim.api.nvim_buf_get_mark(0, '"')
+		local lcount = vim.api.nvim_buf_line_count(0)
+		if mark[1] > 0 and mark[1] <= lcount then
+			pcall(vim.api.nvim_win_set_cursor, 0, { mark[1], mark[2] })
+		end
+	end,
+})
