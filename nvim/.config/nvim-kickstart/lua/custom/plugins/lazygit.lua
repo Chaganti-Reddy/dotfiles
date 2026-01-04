9147be61adaddb442600031069af6bfe8f0812e3
Chaganti-Reddy <chagantivenkataramireddy4@gmail.com>
2026-01-04 18:24:01 +0530

    create nvim/.config/nvim-kickstart/lua/custom/plugins/lazygit.lua

---
+++ nvim/.config/nvim-kickstart/lua/custom/plugins/lazygit.lua
@@ -0,0 +1,30 @@
+return {
+  'kdheepak/lazygit.nvim',
+  cmd = {
+    'LazyGit',
+    'LazyGitConfig',
+    'LazyGitCurrentFile',
+    'LazyGitFilter',
+    'LazyGitFilterCurrentFile',
+  },
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+  },
+  keys = {
+    -- We use <leader>lg so it shows up under the [L]azyGit group we named earlier
+    {
+      '<leader>gg',
+      '<cmd>LazyGit<cr><cmd>hi LazyGitFloat guibg=NONE guifg=NONE<cr><cmd>setlocal winhl=NormalFloat:LazyGitFloat<cr>',
+      desc = 'LazyGit Open',
+    },
+  },
+  config = function()
+    vim.g.lazygit_floating_window_winblend = 0
+    vim.g.lazygit_floating_window_scaling_factor = 0.9
+    vim.g.lazygit_floating_window_border_chars = { '╭', '─', '╮', '│', '╯', '─', '╰', '│' }
+    vim.g.lazygit_floating_window_use_plenary = 0
+    vim.g.lazygit_use_neovim_remote = 1
+    vim.g.lazygit_use_custom_config_file_path = 0
+    vim.g.lazygit_config_file_path = {}
+  end,
+}
