3a1ca6074c3022b3c9b222de2fa088b79c5d5fe5
Chaganti-Reddy <chagantivenkataramireddy4@gmail.com>
2026-01-04 18:24:01 +0530

    update nvim/.config/nvim-kickstart/lua/kickstart/plugins/gitsigns.lua

--- nvim/.config/nvim-kickstart/lua/kickstart/plugins/gitsigns.lua
+++ nvim/.config/nvim-kickstart/lua/kickstart/plugins/gitsigns.lua
@@ -1,11 +1,15 @@
--- Adds git related signs to the gutter, as well as utilities for managing changes
--- NOTE: gitsigns is already included in init.lua but contains only the base
--- config. This will add also the recommended keymaps.
-
 return {
   {
     'lewis6991/gitsigns.nvim',
     opts = {
+      preview_config = {
+        border = 'rounded',
+        style = 'minimal',
+        relative = 'cursor',
+        row = 0,
+        col = 1,
+        zindex = 45,
+      },
       on_attach = function(bufnr)
         local gitsigns = require 'gitsigns'
 
@@ -33,28 +37,43 @@ return {
         end, { desc = 'Jump to previous git [c]hange' })
 
         -- Actions
-        -- visual mode
-        map('v', '<leader>hs', function()
+        -- Visual mode: stage/reset hunks
+        map('v', '<leader>ghs', function()
           gitsigns.stage_hunk { vim.fn.line '.', vim.fn.line 'v' }
         end, { desc = 'git [s]tage hunk' })
-        map('v', '<leader>hr', function()
+        map('v', '<leader>ghr', function()
           gitsigns.reset_hunk { vim.fn.line '.', vim.fn.line 'v' }
         end, { desc = 'git [r]eset hunk' })
-        -- normal mode
-        map('n', '<leader>hs', gitsigns.stage_hunk, { desc = 'git [s]tage hunk' })
-        map('n', '<leader>hr', gitsigns.reset_hunk, { desc = 'git [r]eset hunk' })
-        map('n', '<leader>hS', gitsigns.stage_buffer, { desc = 'git [S]tage buffer' })
-        map('n', '<leader>hu', gitsigns.stage_hunk, { desc = 'git [u]ndo stage hunk' })
-        map('n', '<leader>hR', gitsigns.reset_buffer, { desc = 'git [R]eset buffer' })
-        map('n', '<leader>hp', gitsigns.preview_hunk, { desc = 'git [p]review hunk' })
-        map('n', '<leader>hb', gitsigns.blame_line, { desc = 'git [b]lame line' })
-        map('n', '<leader>hd', gitsigns.diffthis, { desc = 'git [d]iff against index' })
-        map('n', '<leader>hD', function()
-          gitsigns.diffthis '@'
+
+        -- Normal mode
+        map('n', '<leader>ghs', gitsigns.stage_hunk, { desc = 'git [s]tage hunk' })
+        map('n', '<leader>ghr', gitsigns.reset_hunk, { desc = 'git [r]eset hunk' })
+        map('n', '<leader>ghS', gitsigns.stage_buffer, { desc = 'git [S]tage buffer' })
+
+        -- FIXED: Use undo_stage_hunk instead of stage_hunk
+        map('n', '<leader>ghu', gitsigns.undo_stage_hunk, { desc = 'git [u]ndo stage hunk' })
+
+        map('n', '<leader>ghR', gitsigns.reset_buffer, { desc = 'git [R]eset buffer' })
+        map('n', '<leader>ghp', gitsigns.preview_hunk, { desc = 'git [p]review hunk' })
+
+        -- FIXED: Wrapped in a function() to prevent the "got table" crash
+        map('n', '<leader>ghb', function()
+          gitsigns.blame_line { full = true, focusable = true }
+        end, { desc = 'git [b]lame line' })
+
+        map('n', '<leader>ghd', gitsigns.diffthis, { desc = 'git [d]iff against index' })
+        map('n', '<leader>ghD', function()
+          gitsigns.diffthis '~' -- '~' is more stable than '@' for last commit
         end, { desc = 'git [D]iff against last commit' })
+
         -- Toggles
-        map('n', '<leader>tb', gitsigns.toggle_current_line_blame, { desc = '[T]oggle git show [b]lame line' })
-        map('n', '<leader>tD', gitsigns.preview_hunk_inline, { desc = '[T]oggle git show [D]eleted' })
+        map('n', '<leader>tb', gitsigns.toggle_current_line_blame, { desc = '[T]oggle git [b]lame' })
+
+        -- FIXED: toggle_deleted is the proper toggle for ghost lines
+        map('n', '<leader>td', gitsigns.toggle_deleted, { desc = '[T]oggle git show [d]eleted' })
+
+        -- Added: Word diff is incredibly helpful for seeing exact character changes
+        map('n', '<leader>tw', gitsigns.toggle_word_diff, { desc = '[T]oggle git [w]ord diff' })
       end,
     },
   },
