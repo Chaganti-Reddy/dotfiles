97edb46136ab5a9237214a524cee84813a131f10
Chaganti-Reddy <chagantivenkataramireddy4@gmail.com>
2026-01-04 23:58:27 +0530

    create nvim/.config/nvim/lua/kickstart/

---
+++ nvim/.config/nvim/lua/kickstart/plugins/gitsigns.lua
@@ -0,0 +1,77 @@
+return {
+  {
+    'lewis6991/gitsigns.nvim',
+    opts = {
+      preview_config = {
+        border = 'rounded',
+        style = 'minimal',
+        relative = 'cursor',
+        row = 0,
+        col = 1,
+        zindex = 45,
+      },
+      on_attach = function(bufnr)
+        local gitsigns = require 'gitsigns'
+
+        local function map(mode, l, r, opts)
+          opts = opts or {}
+          opts.buffer = bufnr
+          vim.keymap.set(mode, l, r, opts)
+        end
+
+        -- Navigation
+        map('n', ']c', function()
+          if vim.wo.diff then
+            vim.cmd.normal { ']c', bang = true }
+          else
+            gitsigns.nav_hunk 'next'
+          end
+        end, { desc = 'Jump to next git [c]hange' })
+
+        map('n', '[c', function()
+          if vim.wo.diff then
+            vim.cmd.normal { '[c', bang = true }
+          else
+            gitsigns.nav_hunk 'prev'
+          end
+        end, { desc = 'Jump to previous git [c]hange' })
+
+        -- Actions
+        -- Visual mode: stage/reset hunks
+        map('v', '<leader>ghs', function()
+          gitsigns.stage_hunk { vim.fn.line '.', vim.fn.line 'v' }
+        end, { desc = 'git [s]tage hunk' })
+        map('v', '<leader>ghr', function()
+          gitsigns.reset_hunk { vim.fn.line '.', vim.fn.line 'v' }
+        end, { desc = 'git [r]eset hunk' })
+
+        -- Normal mode
+        map('n', '<leader>ghs', gitsigns.stage_hunk, { desc = 'git [s]tage hunk' })
+        map('n', '<leader>ghr', gitsigns.reset_hunk, { desc = 'git [r]eset hunk' })
+        map('n', '<leader>ghS', gitsigns.stage_buffer, { desc = 'git [S]tage buffer' })
+
+        -- FIXED: Use undo_stage_hunk instead of stage_hunk
+        map('n', '<leader>ghu', gitsigns.undo_stage_hunk, { desc = 'git [u]ndo stage hunk' })
+
+        map('n', '<leader>ghR', gitsigns.reset_buffer, { desc = 'git [R]eset buffer' })
+        map('n', '<leader>ghp', gitsigns.preview_hunk, { desc = 'git [p]review hunk' })
+
+        map('n', '<leader>ghb', function()
+          gitsigns.blame_line { full = true }
+        end, { desc = 'git [b]lame line' })
+
+        map('n', '<leader>ghd', gitsigns.diffthis, { desc = 'git [d]iff against index' })
+        map('n', '<leader>ghD', function()
+          gitsigns.diffthis '~' -- '~' is more stable than '@' for last commit
+        end, { desc = 'git [D]iff against last commit' })
+
+        -- Toggles
+        -- FIXED: toggle_deleted is the proper toggle for ghost lines
+        map('n', '<leader>td', gitsigns.toggle_deleted, { desc = '[T]oggle git show [d]eleted' })
+
+        -- Added: Word diff is incredibly helpful for seeing exact character changes
+        map('n', '<leader>tw', gitsigns.toggle_word_diff, { desc = '[T]oggle git [w]ord diff' })
+      end,
+    },
+  },
+}
