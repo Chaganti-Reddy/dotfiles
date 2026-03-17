59408fdfbdb762b6aab06382e91fe19c35117f9b
Chaganti-Reddy <chagantivenkataramireddy4@gmail.com>
2026-03-17 10:21:54 +0530

    refactor: update lint.lua

--- nvim/.config/nvim/lua/kickstart/plugins/lint.lua
+++ nvim/.config/nvim/lua/kickstart/plugins/lint.lua
@@ -1,3 +1,5 @@
+---@module 'lazy'
+---@type LazySpec
 return {
 
   { -- Linting
@@ -56,20 +58,16 @@ return {
       -- Create autocommand which carries out the actual linting
       -- on the specified events.
       local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
-      vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave', 'TextChanged' }, {
+      vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
         group = lint_augroup,
         callback = function()
           -- Only run the linter in buffers that you can modify in order to
           -- avoid superfluous noise, notably within the handy LSP pop-ups that
           -- describe the hovered symbol using Markdown.
-          if vim.bo.modifiable then
-            lint.try_lint()
-          end
+          if vim.bo.modifiable then lint.try_lint() end
         end,
       })
-      vim.keymap.set('n', '<leader>cl', function()
-        lint.try_lint()
-      end, { desc = '[C]ode [L]int' })
+      vim.keymap.set('n', '<leader>cl', function() lint.try_lint() end, { desc = '[C]ode [L]int' })
     end,
   },
 }
